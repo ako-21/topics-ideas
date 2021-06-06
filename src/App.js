da6e5c29dbe5692bfa1c309999949d6bdc84afbf
ako-21 <akeemogle@gmail.com>
2021-06-06 12:36:08 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import ClusterDiagramContainer from './Components/ClusterDiagram/ClusterDiagram';
+import FreeWritingEditorContainer from './Components/FreeWritingEditorContainer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>Cluster Diagram</header>
-      <ClusterDiagramContainer />
+      <FreeWritingEditorContainer />
     </div>
   );
 }
